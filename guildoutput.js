a6367c2f33a012656290869b70314e7c1e8ee32e
Sandy <pimpcow@gmail.com>
2016-09-24 10:25:58 -0400

    Formatting / 2 outputs
    the rank output had been commented out so it's been placed back in.
    methods are chosen by using 0 or 1 for either rank or sorted by
    achievement points (to find alts)

--- guildoutput.js
+++ guildoutput.js
@@ -1,94 +1,79 @@
 /* ***********************************
- ***     Copyright (c) 2016 bruk
+ ***     Copyright (c) 2015 bruk
  *** This script is free software; you can redistribute it and/or modify
  *** it under the terms of the GNU General Public License as published by
  *** the Free Software Foundation; either version 3 of the License, or
  *** (at your option) any later version.
- **********************************  */
+   **********************************  */
 
 
-// Returns entire guild, can be called and used in two ways:
-// 1.) By rank
-//    formula usage =transpose(guild(region, realm, guildname))
-//
-// 2.) By Achievement points (to try to find unique players)
-//    formula usage =guild(region, realm, guildname,1)
-// example template: https://docs.google.com/spreadsheets/d/1LtPZrrRTpgDE2lDQL2BGSdCEQW4Ub82lmJYarEaTqZ8/edit?usp=sharing
-//
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ IMPORTANT!!! ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-//    You need to put your api key here
-//    Request one here: https://dev.battle.net/apps/register
-//    Step by step instructions: http://bruk.org/api
-//   if you have this as part of a combined spreadsheet you can comment it out instead
-/* globals UrlFetchApp */
-/* exported guild */
+// formula usage =guild(region, realm, guildname, outputMethod)
+// outputMethod should be 0 or 1
+// method 0 = output by rank
+// method 1 = output by achievement points, to find alts
 
+// enter your api key here:
+// if you have this as part of a combined spreadsheet you can comment this line out
 var apikey = "";
 
 
-function sortFunction(a, b)
+function sortFunction(a, b) 
 {
-    if (a[0] === b[0])
+    if (a[0] === b[0]) 
     {
         return 0;
     }
-    else
-    {
-        return (a[0] < b[0]) ? -1 : 1;
+    else {
+        return (a[0] > b[0]) ? -1 : 1;
     }
 }
 
-
-function guild(region,realmName,guildName,opt_sort)
+function guildOut(region,realmName,guildName,outputMethod) 
 {
-
+  
     if (!guildName || !realmName )
     {
         return "\u2063";  // If there's nothing don't even bother calling the API
     }
 
-    //Getting rid of any sort of pesky no width white spaces we may run into
-    // XXX: Unused variable?
-    //var toonName = guildName.replace(/[\u200B-\u200D\uFEFF]/g, "");
-
+//Getting rid of any sort of pesky no width white spaces we may run into
     region = region.replace(/[\u200B-\u200D\uFEFF]/g, "");
     realmName = realmName.replace(/[\u200B-\u200D\uFEFF]/g, "");
-
-
-    var guildJSON = UrlFetchApp.fetch("https://"+region+".api.battle.net/wow/guild/"+realmName+"/"+guildName+"?fields=members&?locale=en_US&apikey="+apikey+"");
-    var guildData = JSON.parse(guildJSON);
-    // XXX: Unused variable?
-    //var memberTotal = guildData.members.length;
-    var membermatrix = [ ];
-    var i = 0;
-
-    // sort by number of cheevo points
-    if (opt_sort)
+  
+    if (outputMethod != 0 && outputMethod != 1)
     {
-        for (i=0; i<guildData.members.length; i++)
-        {
-            membermatrix[i] = [guildData.members[i].character.achievementPoints, guildData.members[i].character.name, guildData.members[i].rank];
-        }
-
-        membermatrix.sort(sortFunction);  //  membermatrix[].sort(function(a, b){return b-a});
-
+        return "Invalid outputMethod, please select 0 for rank output, or 1 for alt finder";
     }
-    // sort by rank
+  
+    var guildJSON = UrlFetchApp.fetch("https://"+region+".api.battle.net/wow/guild/"+realmName+"/"+guildName+"?fields=members&?locale=en_US&apikey="+apikey+"");
+
+    var guild = JSON.parse(guildJSON);
+
+    var membermatrix = [ ]; 
+    var rank = 0;
+  
+    if (outputMethod === 1)
+   {
+        for (var i=0; i<guild.members.length; i++)
+       {
+            membermatrix[i] = [guild.members[i].character.achievementPoints, guild.members[i].character.name, guild.members[i].rank];
+        }
+        membermatrix.sort(sortFunction);
+    }
     else
     {
-        var rank;
-
         for (i=0; i<10; i++)
         {
             membermatrix[i] = [];
         }
 
-        for (i=0; i<guildData.members.length; i++)
-        {
-            rank=guildData.members[i].rank;
-            membermatrix[rank].push(guildData.members[i].character.name);
+        for (i=0; i<guild.members.length; i++)
+       {
+            rank=guild.members[i].rank;
+            membermatrix[rank].push(guild.members[i].character.name);
         }
     }
 
     return membermatrix;
+  
 }
