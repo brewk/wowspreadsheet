230df428536af060ecb85cdb5aa623fd714069b0
brewk <brukwow@gmail.com>
2020-03-21 14:24:00 -0400

    regex fix
    fixed issue for realms with hyphens not working properly and cleaned up
    regex a bit

--- wow_bfa.js
+++ wow_bfa.js
@@ -191,7 +191,7 @@ function wow(region,toonName,realmName)
 function fixNames(reg,realm,toon)
 {
     reg = reg.replace(/\s/g, "").toLowerCase();
-    realm = realm.replace(/[\u200B-\u200D\uFEFF]/g, "").replace("'", "").replace(/\s/g, "-").toLowerCase();
+    realm = realm.replace(/[\u200B-\u200D\uFEFF'-]/g, "").replace(/\s/g, "-").toLowerCase();
     toon = toon.toLowerCase().replace(/\s/g, "-");
 
     return {
