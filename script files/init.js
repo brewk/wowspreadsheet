3b5aec18f8a92ddbe05d828d4e3f1284ae134f2a
SKSniperSK <sksn1p3r@gmail.com>
2020-11-29 17:55:19 +0100

    Adjustments for centralized data Changed raiderIO / M+ code Minor fixes

--- script files/init.js
+++ script files/init.js
@@ -8,125 +8,138 @@
  * function to build global environment
  */
 function buildEnv() {
-    // build environment
-    try {
-      this.Settings = appSettings({
-        blizzClientId: '',
-        blizzClientSecret: '',
-        useRaiderIoData: false,
-        epicGemIlvl: 120,
-        auditIlvl: 100,
-        markLegendary: true,
-      });
-      this.Utils = appUtils({
-        settings: this.Settings,
-      });
-      this.BlizzData = appBlizzData({
-        settings: this.Settings,
-        utils: this.Utils,
-      });
-      this.WowGuildRoster = appWowGuildRoster({
-        settings: this.Settings,
-        utils: this.Utils,
-        blizzData: this.BlizzData,
-        blacklist: [],
-        whitelist: [],
-        nonGuild: [],
-        rankBlacklist: [],
-        rankWhitelist: [],
-      });
-      this.WowSl = appWowSl({
-        settings: this.Settings,
-        utils: this.Utils,
-        blizzData: this.BlizzData,
-      });
-    } catch (error) {
-      return false;
-    }
-    return true;
+  // build environment
+  try {
+    this.Utils = appUtils({});
+    this.Settings = appSettings({
+      utils: this.Utils,
+      blizzClientId: '',
+      blizzClientSecret: '',
+      useRaiderIoData: true,
+      epicGemIlvl: 120,
+      auditIlvl: 100,
+      markLegendary: true,
+    });
+    this.RaiderIO = appWowRaiderIO({
+      settings: this.Settings,
+      utils: this.Utils,
+    });
+    this.BlizzData = appBlizzData({
+      settings: this.Settings,
+      utils: this.Utils,
+    });
+    this.WowGuildRoster = appWowGuildRoster({
+      settings: this.Settings,
+      utils: this.Utils,
+      blizzData: this.BlizzData,
+      blacklist: [],
+      whitelist: [],
+      nonGuild: [],
+      rankBlacklist: [],
+      rankWhitelist: [],
+    });
+    this.WowChar = appWowSl({
+      settings: this.Settings,
+      utils: this.Utils,
+      blizzData: this.BlizzData,
+      raiderIO: this.RaiderIO,
+    });
+  } catch (error) {
+    return false;
   }
-  
-  /**
-   * wrapper function to call object functions from within sheet cells
-   * @param {string} objDotFunc The function name including object/namespace ('Object.Function')
-   * @param {array} args The function arguments, variable length
-   * @return Function result, variable type.
-   */
-  function appCall(objDotFunc, ...args) {
-    if (buildEnv()) {
-      Logger.log(objDotFunc, args);
-      const arr = objDotFunc.split('.'); // a string to array with "." as separator
-      const obj = arr[0]; // getting object name
-      const func = arr[1]; // getting function name
-      return this[obj][func].apply(this, args);
-    }
-    return 'Error building environment';
+  return true;
+}
+
+/**
+ * wrapper function to call object functions from within sheet cells
+ * @param {string} objDotFunc The function name including object/namespace ('Object.Function')
+ * @param {array} args The function arguments, variable length
+ * @return Function result, variable type.
+ */
+function appCall(objDotFunc, ...args) {
+  if (buildEnv()) {
+    Logger.log(objDotFunc, args);
+    const arr = objDotFunc.split('.'); // a string to array with "." as separator
+    const obj = arr[0]; // getting object name
+    const func = arr[1]; // getting function name
+    return this[obj][func].apply(this, args);
   }
-  
-  /**
-   * onOpen simple trigger
-   */
-  function onOpen() {
-    // create menu
-    const sheet = SpreadsheetApp.getActiveSpreadsheet();
-    const entries = [
-      { name: 'Refresh All', functionName: 'refreshAll' },
-      { name: 'Refresh Gear', functionName: 'refreshGear' },
-      { name: 'Refresh Progression', functionName: 'refreshProg' },
-      { name: 'Refresh Professions', functionName: 'refreshProf' },
-      { name: 'Refresh Reputation', functionName: 'refreshRep' },
-    ];
-    sheet.addMenu('\u21BB  Refresh all Characters', entries);
-  }
-  
-  /**
-   * helper function that writes the current time in the provided cell
-   * @param {string} cell the cell to update in A1 notation
-   */
-  function refreshCell(cell) {
-    SpreadsheetApp.getActiveSpreadsheet().getRange(cell).setValue(new Date().toTimeString());
-  }
-  
-  /**
-   * function used by custom menu to force a refresh
-   */
-  function refreshAll() {
-    refreshCell('A1');
-  }
-  
-  /**
-   * function used by custom menu to force a refresh
-   */
-  function refreshGear() {
-    refreshCell('AI4');
-  }
-  
-  /**
-   * function used by custom menu to force a refresh
-   */
-  function refreshProg() {
-    refreshCell('AY4');
-  }
-  
-  /**
-   * function used by custom menu to force a refresh
-   */
-  function refreshProf() {
-    refreshCell('CQ4');
-  }
-  
-  /**
-   * function used by custom menu to force a refresh
-   */
-  function refreshRep() {
-    refreshCell('CS4');
-  }
-  
-  /**
-   * function used to debug stuff
-   */
-  function debug() {
-    buildEnv();
-    this.BlizzData.getCharData('eu', 'eredar', 'dakof', 'charProfile');
-  }
-  
+  return 'Error building environment';
+}
+
+/**
+ * onOpen simple trigger
+ */
+function onOpen() {
+  // create menu
+  const sheet = SpreadsheetApp.getActiveSpreadsheet();
+  const entries = [
+    { name: 'Refresh All', functionName: 'refreshAll' },
+    { name: 'Refresh Gear', functionName: 'refreshGear' },
+    { name: 'Refresh Progression', functionName: 'refreshProg' },
+    { name: 'Refresh Professions', functionName: 'refreshProf' },
+    { name: 'Refresh Reputation', functionName: 'refreshRep' },
+  ];
+  sheet.addMenu('\u21BB  Refresh all Characters', entries);
+}
+
+/**
+ * helper function that writes the current time in the provided cell
+ * @param {string} cell the cell to update in A1 notation
+ */
+function refreshCell(cell) {
+  SpreadsheetApp.getActiveSpreadsheet().getRange(cell).setValue(new Date().toTimeString());
+}
+
+/**
+ * function used by custom menu to force a refresh
+ */
+function refreshAll() {
+  refreshCell('A1');
+}
+
+/**
+ * function used by custom menu to force a refresh
+ */
+function refreshGear() {
+  refreshCell('AI4');
+}
+
+/**
+ * function used by custom menu to force a refresh
+ */
+function refreshProg() {
+  refreshCell('AY4');
+}
+
+/**
+ * function used by custom menu to force a refresh
+ */
+function refreshProf() {
+  refreshCell('CQ4');
+}
+
+/**
+ * function used by custom menu to force a refresh
+ */
+function refreshRep() {
+  refreshCell('CS4');
+}
+
+/**
+ * function to create compressed string of data
+ * @param {array} data data to compress
+ * @returns {string} compressed data
+ */
+function compress(data) {
+  const myUtils = appUtils({});
+  return myUtils.zippedStringArray(JSON.stringify(data));
+}
+
+/**
+ * function used to debug stuff
+ */
+function debug() {
+  buildEnv();
+  this.BlizzData.getCharData('eu', 'eredar', 'dakof', 'charProfile');
+}
---
+++ script files/raiderIO.js
@@ -0,0 +1,160 @@
+/**
+ * @OnlyCurrentDoc
+ */
+
+/* globals appSettings, appUtils */
+
+/**
+ * The appWowRaiderIO object
+ * @param {Object} par The main parameter object.
+ * @return {Object} The WowRaiderIO Object.
+ */
+function appWowRaiderIO(par) {
+  const objectName = 'appWowRaiderIO';
+  const currentVersionRaiderIO = 1.0;
+  const mySettings = par.settings || appSettings();
+  const myUtils = par.utils || appUtils();
+  const cache = CacheService.getScriptCache();
+  const useRaiderIo = mySettings.useRaiderIoData || false;
+  const strRaiderIOSheetName = 'Mythic+';
+  const strRaiderIOSheetCache = 'rioSheetCache';
+  const strRaiderIoNotEnabledError = 'RaiderIO data not enabled in settings';
+  const strApiError = 'Error: invalid data received from API';
+
+  /**
+   * function to get current m+ affix for specifig region
+   * @param {string} region region of where to get affixes for
+   * @return {string} comma delimited string containing all current affixes
+   */
+  function getAffixes(region) {
+    if (!useRaiderIo) {
+      return strRaiderIoNotEnabledError;
+    }
+
+    let affixes = '';
+
+    const fetchUrl = `https://raider.io/api/v1/mythic-plus/affixes?region=${region}&locale=en`;
+
+    let data;
+    try {
+      data = myUtils.jsonFetch(fetchUrl);
+    } catch (e) {
+      return `Error getting RaiderIO data (${e.message})`;
+    }
+    if (data && data.title) {
+      affixes = data.title;
+    } else {
+      return strApiError;
+    }
+
+    return affixes;
+  }
+
+  /**
+   * helper function to parse raiderIO API data for runs
+   * @param {any} data JSON object raiderIO API data containing run infos
+   * @return {any} output object containing all relevant run infos
+   */
+  function parseRunData(data) {
+    // default value
+    const runInfo = { mythicLevel: 0, dungeon: '-', affixes: ['-', '-', '-', '-'], numKeystoneUpgrades: 0, score: 0 };
+
+    if (data && data[0]) {
+      const run = data[0];
+      runInfo.mythicLevel = run.mythic_level;
+      runInfo.dungeon = run.dungeon;
+      runInfo.numKeystoneUpgrades = run.num_keystone_upgrades;
+      runInfo.score = run.score;
+
+      for (i = 0; i < run.affixes.length; i++) {
+        var affix = run.affixes[i].name;
+        runInfo.affixes[i] = affix.substring(0, 3).toUpperCase();
+      }
+    }
+
+    return runInfo;
+  }
+
+  /**
+   * function to get all m+ raiderIO data for a toon
+   * @param {string} region region of target toon
+   * @param {string} realmName realm of target toon
+   * @param {string} toonName name of target toon
+   * @return {array} toon raiderIO data
+   */
+  function getFullData(region, realmName, toonName) {
+    if (!useRaiderIo) {
+      return strRaiderIoNotEnabledError;
+    }
+
+    if (!toonName || !realmName) {
+      return ' '; // If there's nothing in the column, don't even bother calling the API
+    }
+
+    toonName = myUtils.lowerCaseAllWordsExceptFirstLetters(toonName.toUpperCase());
+    const fetchUrl = `https://raider.io/api/v1/characters/profile?region=${region}&realm=${realmName}&name=${toonName}&fields=mythic_plus_scores%2Cmythic_plus_recent_runs%2Cmythic_plus_best_runs%2Cmythic_plus_highest_level_runs%2Cmythic_plus_weekly_highest_level_runs`;
+
+    let data;
+    try {
+      data = myUtils.jsonFetch(fetchUrl);
+    } catch (e) {
+      return myUtils.initializedArray(31, `Error getting RaiderIO data (${e.message})`);
+    }
+    if (!data || !data.profile_url) {
+      return myUtils.initializedArray(31, strApiError);
+    }
+
+    const profile = data.profile_url;
+    const thumbnail = data.thumbnail_url;
+    const scoreAll = data.mythic_plus_scores.all;
+
+    // parsing data for runs
+    const weeklyHighest = parseRunData(data.mythic_plus_weekly_highest_level_runs);
+    const recentRun = parseRunData(data.mythic_plus_recent_runs);
+    const highestRun = parseRunData(data.mythic_plus_highest_level_runs);
+    const bestRun = parseRunData(data.mythic_plus_best_runs);
+
+    const toonInfo = [
+      thumbnail,
+      profile,
+      scoreAll,
+
+      weeklyHighest.mythicLevel,
+      weeklyHighest.dungeon,
+      weeklyHighest.score,
+      weeklyHighest.numKeystoneUpgrades,
+      recentRun.mythicLevel,
+      recentRun.dungeon,
+      ...recentRun.affixes,
+      recentRun.score,
+      recentRun.numKeystoneUpgrades,
+      highestRun.mythicLevel,
+      highestRun.dungeon,
+      ...highestRun.affixes,
+      highestRun.score,
+      highestRun.numKeystoneUpgrades,
+      bestRun.mythicLevel,
+      bestRun.dungeon,
+      ...bestRun.affixes,
+      bestRun.score,
+      bestRun.numKeystoneUpgrades,
+    ];
+
+    return toonInfo;
+  }
+
+  /**
+   * function to get the version of the guild module
+   * @return {number} version of guild module
+   */
+  function verCheckRaiderIO() {
+    return currentVersionRaiderIO;
+  }
+
+  return Object.freeze({
+    objectName,
+    getAffixes,
+    getFullData,
+    verCheckRaiderIO,
+  });
+}
